dd27abd129705b77d1cf2575e656bf4d3dab79ac
Jansora <zhangyue1936@gmail.com>
2022-08-11 19:52:26 +0800

    init base:v6

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -1,7 +1,7 @@
-#docker build -t jansora/base:v6 .
+docker build -t jansora/base:v6 .
 docker build -t ccr.ccs.tencentyun.com/jansora/base:v6 .
 
 docker push ccr.ccs.tencentyun.com/jansora/base:v6
-#docker push jansora/base:v6
+docker push jansora/base:v6
 
 
