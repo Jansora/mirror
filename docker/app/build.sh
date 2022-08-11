2d754678cd9e8dfc662acf2980e16343b07e3a6c
Jansora <zhangyue1936@gmail.com>
2022-08-11 16:44:47 +0800

    .

--- docker/app/build.sh
+++ docker/app/build.sh
@@ -1,5 +1,5 @@
 docker build -t jansora/app:v1 .
-
+docker build -t ccr.ccs.tencentyun.com/jansora/app:v1 .
 docker push ccr.ccs.tencentyun.com/jansora/app:v1
 docker push jansora/app:v1
 
--- docker/base/build.sh
+++ docker/base/build.sh
@@ -1,8 +1,7 @@
-
 docker build -t jansora/base:v6 .
-
+docker build -t ccr.ccs.tencentyun.com/jansora/base:v6 .
 
 docker push ccr.ccs.tencentyun.com/jansora/base:v6
-docker push base:v6
+docker push jansora/base:v6
 
 
