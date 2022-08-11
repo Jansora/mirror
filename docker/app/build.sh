198e05945f240048e544734087928c9f0c01d519
Jansora <zhangyue1936@gmail.com>
2022-08-11 19:59:50 +0800

    init app

--- docker/app/build.sh
+++ docker/app/build.sh
@@ -1,4 +1,4 @@
-#docker build -t jansora/app:v1 .
+docker build -t jansora/app:v1 .
 docker build -t ccr.ccs.tencentyun.com/jansora/app:v1 .
 docker push ccr.ccs.tencentyun.com/jansora/app:v1
 #docker push jansora/app:v1
