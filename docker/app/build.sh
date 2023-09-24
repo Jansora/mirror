23d18e16b31c5d02ff9895c6dfabbe5a3517390f
zhangyy53 <zhangyue1936@gmail.com>
2023-09-24 22:10:02 +0800

    add mirror

--- docker/app/build.sh
+++ docker/app/build.sh
@@ -1,6 +1,11 @@
-docker build -t jansora/app:v1 .
-docker build -t ccr.ccs.tencentyun.com/jansora/app:v1 .
-docker push ccr.ccs.tencentyun.com/jansora/app:v1
+#docker build -t jansora/app:v1 .
+#docker build -t ccr.ccs.tencentyun.com/jansora/app:v1 .
+#docker push ccr.ccs.tencentyun.com/jansora/app:v1
 #docker push jansora/app:v1
+
+docker build -t jansora/app:v2  .
+docker build -t ccr.ccs.tencentyun.com/jansora/app:v2 .
+docker push ccr.ccs.tencentyun.com/jansora/app:v2
+#docker push jansora/app:v2
 
 
