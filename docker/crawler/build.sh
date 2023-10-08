13a48af017fb7acda98e9e69381af315993840f3
zhangyy53 <zhangyue1936@gmail.com>
2023-10-08 10:10:52 +0800

    add crawler

--- docker/crawler/build.sh
+++ docker/crawler/build.sh
@@ -1,7 +1,7 @@
 docker build -t ghcr.io/jansora/crawler:v1 .
 docker build -t jansora/crawler:v1 .
+docker build -t ccr.ccs.tencentyun.com/jansora/crawler:v1 .
 
 docker push ghcr.io/jansora/crawler:v1
 docker push jansora/crawler:v1
-
-
+docker push ccr.ccs.tencentyun.com/jansora/crawler:v1
