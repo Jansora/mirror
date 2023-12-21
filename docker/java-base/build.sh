b5ee2423c84c2c4c3c2c57e599c6ec0f2e8bd929
zhangyy53 <zhangyue1936@gmail.com>
2023-12-21 14:36:15 +0800

    update java-base to v1 / add arthas

--- docker/java-base/build.sh
+++ docker/java-base/build.sh
@@ -4,7 +4,7 @@
 #docker push ccr.ccs.tencentyun.com/jansora/base:v6
 #docker push jansora/base:v6
 
-docker build -t jansora/base:v3 .
-docker tag jansora/base:v3 registry.cn-hongkong.aliyuncs.com/jansora/base:v3
-docker push registry.cn-hongkong.aliyuncs.com/jansora/base:v3
+docker build -t jansora/java-base:v1 .
+docker tag jansora/java-base:v1 registry.cn-hongkong.aliyuncs.com/jansora/java-base:v1
+docker push registry.cn-hongkong.aliyuncs.com/jansora/java-base:v1
 #docker build -t registry.cn-hongkong.aliyuncs.com/jansora/base:v3 .
