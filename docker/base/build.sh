a311f8fa76aacdfd7335ad9b2205c3873ed4b649
zhangyy53 <zhangyue1936@gmail.com>
2023-12-21 14:29:51 +0800

    update base

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -1,7 +1,10 @@
-docker build -t jansora/base:v6 .
-docker build -t ccr.ccs.tencentyun.com/jansora/base:v6 .
+#docker build -t jansora/base:v6 .
+#docker build -t ccr.ccs.tencentyun.com/jansora/base:v6 .
+#
+#docker push ccr.ccs.tencentyun.com/jansora/base:v6
+#docker push jansora/base:v6
 
-docker push ccr.ccs.tencentyun.com/jansora/base:v6
-docker push jansora/base:v6
-
-
+docker build -t jansora/base:v3 .
+docker tag jansora/base:v3 registry.cn-hongkong.aliyuncs.com/jansora/base:v3
+docker push registry.cn-hongkong.aliyuncs.com/jansora/base:v3
+#docker build -t registry.cn-hongkong.aliyuncs.com/jansora/base:v3 .
