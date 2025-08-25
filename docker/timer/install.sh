feb27daa305991ec48f0cb509fca4688ff6b60d9
zhangyy53 <zhangyue1936@gmail.com>
2025-08-25 11:14:29 +0800

    。。。

--- docker/timer/install.sh
+++ docker/timer/install.sh
@@ -1,2 +1,2 @@
-docker build -t ccr.ccs.tencentyun.com/jansora/timer:0.0.1 .
-docker push  ccr.ccs.tencentyun.com/jansora/timer:0.0.1
+docker build -t ccr.ccs.tencentyun.com/jansora/timer:0.0.2 .
+docker push  ccr.ccs.tencentyun.com/jansora/timer:0.0.2
