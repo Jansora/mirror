4b7dddec0088562cb09e363efea545215ef73d21
zhangyy53 <zhangyue1936@gmail.com>
2025-09-17 11:41:41 +0800

    。。。

---
+++ docker/autogen-studio/install.sh
@@ -0,0 +1,5 @@
+#  官方文件
+# https://github.com/microsoft/autogen/blob/autogenstudio/samples/apps/autogen-studio/Dockerfile
+
+docker build -t ccr.ccs.tencentyun.com/jansora/autogenstudio:0.0.1 .
+docker push  ccr.ccs.tencentyun.com/jansora/autogenstudio:0.0.1
