47528cbe1c7b54cf92d8d2373625f3db0941d4f7
zhangyy53 <zhangyue1936@gmail.com>
2025-11-05 17:04:45 +0800

    。。。

---
+++ docker/file-server/install.sh
@@ -0,0 +1,2 @@
+docker build -t ccr.ccs.tencentyun.com/jansora/file-server:0.0.1 .
+docker push  ccr.ccs.tencentyun.com/jansora/file-server:0.0.1
--- docker/timer/install.sh
+++ docker/timer/install.sh
@@ -1,2 +1,2 @@
-docker build -t ccr.ccs.tencentyun.com/jansora/timer:0.0.5 .
-docker push  ccr.ccs.tencentyun.com/jansora/timer:0.0.5
+docker build -t ccr.ccs.tencentyun.com/jansora/timer:0.0.6 .
+docker push  ccr.ccs.tencentyun.com/jansora/timer:0.0.6
