a6270e17a526a21b9216b5e24ed92b93ee019c89
zhangyy53 <zhangyue1936@gmail.com>
2025-11-28 21:38:57 +0800

    。。。

--- docker/mcp/install.sh
+++ docker/mcp/install.sh
@@ -1,2 +1,3 @@
 docker build -t ccr.ccs.tencentyun.com/jansora/mcp:0.0.1 .
 docker push  ccr.ccs.tencentyun.com/jansora/mcp:0.0.1
+skopeo copy docker://ccr.ccs.tencentyun.com/jansora/mcp:0.0.1 docker://ghcr.io/jansora/mcp:0.0.1
