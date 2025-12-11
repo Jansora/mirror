8ccd5b4ebcfcc64ee5e0f595b7742dd567668a84
zhangyy53 <zhangyue1936@gmail.com>
2025-12-11 21:29:00 +0800

    。。。

--- docker/superset/install.sh
+++ docker/superset/install.sh
@@ -1,2 +1,2 @@
-docker build -t ccr.ccs.tencentyun.com/jansora/superset:4.1.1_with_drivers .
-docker push ccr.ccs.tencentyun.com/jansora/superset:4.1.1_with_drivers
+docker build -t ccr.ccs.tencentyun.com/jansora/superset:4.1.1_with_drivers_2 .
+docker push ccr.ccs.tencentyun.com/jansora/superset:4.1.1_with_drivers_2
